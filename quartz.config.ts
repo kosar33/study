54b87e83b99e501ac29d29aa3c7804cc5e15fa80
Илья <106250459+kosar33@users.noreply.github.com>
2026-02-21 12:04:34 +0300

    Update baseUrl to include trailing slash

--- quartz.config.ts
+++ quartz.config.ts
@@ -11,7 +11,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "k33study.duckdns.org",
+    baseUrl: "k33study.duckdns.org/",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
